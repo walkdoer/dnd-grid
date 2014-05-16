ac9ebf27dc1168132ae7ca0db7096139fe8c28b2
zhangmh <zhangmhao@gmail.com>
2014-05-16 14:41:43 +0800

    hover效果

--- dnd/main.js
+++ dnd/main.js
@@ -6,6 +6,7 @@
         totalWidth = dEl.clientWidth,
         totalHeight = dEl.clientHeight,
         $container = $('.container'),
+        counter = {},
         sidebarWidth = 240;
 
     var colNum = 3;
@@ -13,25 +14,33 @@
         width: $container.width(),
         height: totalHeight
     };
-    //列宽
-    var itemWitdh = container.width / colNum;
+    var idGen = function (prefix, spliter) {
+        if (!counter[prefix]) {
+            counter[prefix] = 1;
+        }
+        return [prefix, counter[prefix]++].join(spliter || '-');
+    };
+    var margin = 2,
+        dropAreaList = [],
+        borderWidth = 1,
+        itemWitdh = (container.width - (margin * colNum * 2 + borderWidth * colNum * 2))/ colNum;
     //初始化布局容器
     $container.height(totalHeight);
     for (var i = 0; i < colNum; i++) {
         var $item = $('<div class="drop-area"></div>').css({
-            width: itemWitdh
+            width: itemWitdh,
+            border: borderWidth + 'px solid #ccc',
+            margin: margin
         });
+        var id = idGen('dnd-drop-area');
+        $item.attr('id', id);
+        dropAreaList.push('#' + id);
         $container.append($item);
     }
-    
-    
-
-    
-    
     //取出各个组件
     var $components = $('.sidebar .components li');
-    
     $components.draggable({
+        opacity: 0.35,
         helper: function () {
             var $com = $(this),
                 sizeArr = $com.data('size').split('x'),
@@ -47,12 +56,12 @@
         }
     });
     
-    $('.container .drop-area').droppable({
-        activeClass: 'ui-highlight',
+    $('.drop-area').droppable({
+        hoverClass: 'ui-highlight',
         drop: function (e, ui) {
             var $dragClone = $(ui.draggable).find('.column').clone();
             $(e.target).append($dragClone.removeClass('none'));
         }
-    })
+    });
 
 })(window);
